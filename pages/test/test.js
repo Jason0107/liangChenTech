12d7ccf7be62357b993bdeaed35d95a6d7653f00
liangchen <wj@aduer.com>
2018-06-20 14:48:10 +0800

    initial222

---
+++ pages/test/test.js
@@ -0,0 +1,124 @@
+// // pages/test/test.js
+// Page({
+
+//   /**
+//    * 页面的初始数据
+//    */
+//   data: {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面加载
+//    */
+//   onLoad: function (options) {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面初次渲染完成
+//    */
+//   onReady: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面显示
+//    */
+//   onShow: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面隐藏
+//    */
+//   onHide: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面卸载
+//    */
+//   onUnload: function () {
+
+//   },
+
+//   /**
+//    * 页面相关事件处理函数--监听用户下拉动作
+//    */
+//   onPullDownRefresh: function () {
+
+//   },
+
+//   /**
+//    * 页面上拉触底事件的处理函数
+//    */
+//   onReachBottom: function () {
+
+//   },
+
+//   /**
+//    * 用户点击右上角分享
+//    */
+//   onShareAppMessage: function () {
+
+//   }
+// })
+
+
+
+Page({
+  data: {
+    news: ['第1行信息', '第2行信息', '第3行信息'],
+    btnText: '这是个按钮',
+    text: 'init data',
+    text1: 'text的初始内容',
+    isTextShow: true,
+    num: 0,
+    array: [{
+      text: 'init data'
+    }],
+    object: {
+      text: 'init data'
+    }
+  },
+  changeText: function() {
+    // this.data.text = 'changed data'  // bad, it can not work
+    this.setData({
+      text: 'changed data'
+    })
+  },
+  changeNum: function() {
+    this.data.num = 1
+    // var that =this;
+    this.setData({
+      num: this.data.num
+    })
+  },
+  changeItemInArray: function() {
+    // you can use this way to modify a danamic data path
+    this.setData({
+      'array[0].text': 'changed data'
+    })
+  },
+  changeItemInObject: function() {
+    this.setData({
+      'object.text': 'changed data'
+    });
+  },
+  addNewField: function() {
+    this.setData({
+      'newField.text': 'new data'
+    })
+  },
+  btnClick: function() {
+    this.setData({
+      'btnText': '这个按钮被点击过了'
+    })
+    var isShow = this.data.isTextShow;
+    this.setData({
+      'text1': '按钮被点击，文字被修改',
+      isTextShow : !isShow
+    })
+  }
+})
